7d0c0aff1259448687db5758dd35ff0ef4485498
fierze <vbgbiz@mail.ru>
2022-03-23 14:18:16 +0300

    feat: create StoredFile.cs for Data Model

---
+++ Models/StoredFile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FileServer.Models
+{
+    public class StoredFile
+    {
+        public string Name { get; }
+        public string Extension { get; }
+        public string Type { get; }
+        public string DateUpload { get; }
+        public string TimeUpload { get; }
+        public double Size { get; }
+        public string Path { get; }
+    }
+}
